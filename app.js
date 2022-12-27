db306172518c626df796e5dbdec15b763b8d6c18
ngamsom <setthasiri.ngm@gmail.com>
2022-12-27 12:23:46 +1100

    add comments

--- app.js
+++ app.js
@@ -15,21 +15,29 @@ const server = http.createServer((req,res) => {
     if(url === '/message' && method === 'POST'){
         //Parsing request body
         const body = []
+        //by writing a callback function, we registered the function for Node to execute AFTER.
+        //The order of the code matters. Eg. if the setHeader code get render before the 'end'
+        //callback function, we will have an error as the setHeader is already called and we
+        //can't call it again. We need to make sure that the callback function in 'end' get
+        //execute first.  -- Event Driven Code or Event Driven Architecture
         req.on('data', (chunk) => {
             console.log(chunk)
             body.push(chunk)
         })
-        req.on('end', () => {
+        return req.on('end', () => {
             //parseBody (Buffer) is what we want to use.
             const parseBody = Buffer.concat(body).toString()
             const message = parseBody.split('=')[1]
             console.log(parseBody)
-            fs.writeFileSync('text.txt', message)
+            //writeFileSync vs writeFile - Sync will block all the code after this line until writeFile is done
+            //it should not be use if the file is massive.
+            fs.writeFile('text.txt', message, err => {
+                res.statusCode = 302;
+                //Redirect to the home page
+                res.setHeader('Location', '/')
+                return res.end()
+            })
         })
-        res.statusCode = 302;
-        //Redirect to the home page
-        res.setHeader('Location', '/')
-        return res.end()
     }
 
     res.setHeader('Content-Type', 'text/html')
