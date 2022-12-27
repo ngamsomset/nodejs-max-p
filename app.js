f082ef41330445e99ed66d444aa0094495864894
ngamsom <setthasiri.ngm@gmail.com>
2022-12-27 11:10:33 +1100

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req,res) => {
+    const url = req.url
+    const method = req.method
+
+    if(url === '/') {
+        res.write('<html>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button>Send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('text.txt', 'DUMMY')
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end()
+    }
+
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body><h1>Hello</h1></body>')
+    res.write('</html>')
+    res.end()
+});
+
+server.listen(3000);
