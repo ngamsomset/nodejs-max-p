cd6d70b1fb1868568af4bea0be43b536ae9d6cae
ngamsom <setthasiri.ngm@gmail.com>
2023-01-02 14:04:14 +1100

    add helper function for easier navigation

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
+const rootDir = require('../utils/path')
 
 
 router.get('/', (req,res,next) =>{
     //__dirname hold the absolute path of the project
-    res.sendFile(path.join(__dirname, '../' , 'views', 'shop.html'))
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
 module.exports = router
