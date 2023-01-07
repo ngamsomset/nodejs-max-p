27468f1269d6bc6bdce1234391930a394c255fc3
ngamsom <setthasiri.ngm@gmail.com>
2023-01-07 13:59:35 +1100

    update route to pass data for ejs

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,28 @@
-const express = require('express')
-const router = express.Router()
-const path = require('path')
-const rootDir = require('../utils/path')
+const path = require('path');
 
-const products = []
+const express = require('express');
 
 
-router.get('/add-product', (req,res,next) =>{
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-})
+const router = express.Router();
 
-router.post('/add-product', (req,res,next) =>{
-    products.push({title: req.body.title})
-    res.redirect('/')
-})
+const products = [];
 
-exports.routes = router
-exports.products = products
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,21 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
-const rootDir = require('../utils/path')
-const adminData = require('./admin')
+const path = require('path');
 
+const express = require('express');
 
-router.get('/', (req,res,next) =>{
-    const products = adminData.products
-    res.render('shop', {title: 'Shopxx', products: products})
-})
+const adminData = require('./admin');
 
-module.exports = router
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
+
+module.exports = router;
