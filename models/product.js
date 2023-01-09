5c5dc4f15145ba3a52cbc6b535f4394e1c4563b2
ngamsom <setthasiri.ngm@gmail.com>
2023-01-09 11:51:01 +1100

    refractor model file storage

--- models/product.js
+++ models/product.js
@@ -2,6 +2,29 @@ const fs = require('fs')
 const path = require('path')
 const pathDir = require('../utils/path')
 
+
+
+//This const need to be outside of the helper function
+//because the save() won't have access to it. Thus
+//result in p is not define.
+const p = path.join(pathDir, 'data', 'products.json')
+//helper function. Because we reuse this logic many places
+//it's good to have a function to handle that.
+//1. This function will red the file at fetchAll method
+//2. At save() also need to fetch the existing data
+//   before wrting a new one into it.
+const getProductsFromFile = cb => {
+    //read file from JSON
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([])
+        } else {
+            // fileContent need to parse back from JSON to JS object
+            cb(JSON.parse(fileContent))
+        }
+    })
+}
+
 module.exports = class Product {
     constructor(t){
         this.title = t
@@ -9,18 +32,12 @@ module.exports = class Product {
 
     save() {
         //this will save the data into a json file in /data directory
-        const p = path.join(pathDir, 'data', 'products.json')
-        fs.readFile(p, (err, fileContent) => {
-            let products = []
-            if (!err) {
-                products = JSON.parse(fileContent)
-            }
+        getProductsFromFile(products => {
             products.push(this)
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err)
             })
         })
-
     }
 
     //the reason that we need to pass cb into this fetchAll
@@ -28,14 +45,6 @@ module.exports = class Product {
     //we call products.length too early while there is no data
     //coming in yet.
     static fetchAll(cb){
-        const p = path.join(pathDir, 'data', 'products.json')
-        //read file from JSON
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([])
-            }
-            // fileContent need to parse back from JSON to JS object
-            return cb(JSON.parse(fileContent))
-        })
+      getProductsFromFile(cb)
     }
 }
