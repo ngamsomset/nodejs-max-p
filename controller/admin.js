167a9d12a0e144ea3fc82f37dd1036c944ed7f42
ngamsom <setthasiri.ngm@gmail.com>
2023-01-10 16:54:14 +1100

    add new controller and routes

--- controller/admin.js
+++ controller/admin.js
@@ -16,15 +16,12 @@ exports.getAddProducts = (req, res, next) => {
     });
 }
 
-exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-      });
-    })
-  }
+exports.getAllProducts = (req,res,next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Products',
+      path: '/admin/products',
+    });
+  })
+}
--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,16 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-const productsController = require('../controller/products')
+const adminController = require('../controller/admin')
 
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProducts);
+router.get('/add-product', adminController.getAddProducts);
+
+// /admin/products => GET
+router.get('/products', adminController.getAllProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProducts);
+router.post('/add-product', adminController.postAddProducts);
 
 module.exports = router
