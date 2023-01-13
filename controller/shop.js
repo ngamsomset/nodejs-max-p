a671007a19676675ebafd7b5d394d01d2755b2b7
ngamsom <setthasiri.ngm@gmail.com>
2023-01-14 10:26:22 +1100

    passing data with post request

--- controller/shop.js
+++ controller/shop.js
@@ -38,6 +38,12 @@ exports.getCart = (req,res,next) => {
   })
 }
 
+exports.postCart = (req,res,next) => {
+  const prodId = req.body.productId
+  console.log(prodId)
+  res.redirect('/cart')
+}
+
 exports.checkOut = (req,res,next) => {
   res.render('shop/checkout', {
       path: '/checkout',
