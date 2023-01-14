80055e3c2b8a0064ee7e5e2c6bd4e0d4ac8f30dd
ngamsom <setthasiri.ngm@gmail.com>
2023-01-14 19:36:37 +1100

    adding a cart model

---
+++ models/cart.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const path = require('path')
+const pathDir = require('../utils/path')
+
+const p = path.join(pathDir, 'data', 'cart.json')
+
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        //Fetching a Cart from a file
+        fs.readFile(p, (err, fileContent) => {
+            let cart = {products: [], totalPrice: 0}
+
+            if (!err) {
+                cart = JSON.parse(fileContent)
+            }
+            //Analyzing  the cart => find existing product
+
+            //Get the index of the product Id that we pass in 
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id)
+            const existingProduct = cart.products[existingProductIndex]
+
+            let updatedProduct
+            //check if there is already an existingProduct, if yes we need to replace product and increase QTY.
+            if (existingProduct) {
+            //-set the updatedProduct = to existingProduct
+                updatedProduct = {...existingProduct}
+            //-updateProduct Quantity
+                updatedProduct.qty = updatedProduct.qty + 1
+                cart.products = [...cart.products]
+                cart.products[existingProductIndex] = updatedProduct
+            } else {
+            //if there is no existing product, we want to set the update product to the new productId
+            //copy the existing product in cart and pass the new updatedProduct
+                updatedProduct = {id: id, qty: 1}
+                cart.products = [...cart.products, updatedProduct]
+            }
+            
+            cart.totalPrice = cart.totalPrice + +productPrice
+
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err)
+            })
+        })
+
+
+    }
+}
