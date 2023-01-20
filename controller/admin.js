71771ebcde74765e46e60f21274f382d4e3ee52c
ngamsom <setthasiri.ngm@gmail.com>
2023-01-21 10:17:13 +1100

    comment unusable code for now

--- controller/admin.js
+++ controller/admin.js
@@ -5,13 +5,8 @@ exports.postAddProducts = (req, res, next) => {
     const imageUrl = req.body.imageUrl
     const price = req.body.price
     const description = req.body.description
-   
-    Product.create({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description
-    })
+    const product = new Product(title, imageUrl, price, description, imageUrl)
+    product.save()
     .then((result) => {
       // console.log(result)
       console.log('product created!')
@@ -30,71 +25,71 @@ exports.getAddProducts = (req, res, next) => {
     });
 }
 
-exports.getEditProducts = (req, res, next) => {
-  //check for query params
-  const editMode = req.query.edit
-  if (!editMode) {
-    return res.redirect('/')
-  }
+// exports.getEditProducts = (req, res, next) => {
+//   //check for query params
+//   const editMode = req.query.edit
+//   if (!editMode) {
+//     return res.redirect('/')
+//   }
 
-  const prodId = req.params.productId
-  Product.findByPk(prodId)
-  .then((product) => {
-    res.render('admin/edit-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-}
+//   const prodId = req.params.productId
+//   Product.findByPk(prodId)
+//   .then((product) => {
+//     res.render('admin/edit-product', {
+//       pageTitle: 'Add Product',
+//       path: '/admin/edit-product',
+//       editing: editMode,
+//       product: product
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+// }
 
-exports.getPostEditProducts = async (req,res,next) => {
-  const prodId =  req.body.productId
-  const updatedTitle = req.body.title
-  const updatedPrice = req.body.price
-  const updatedImageUrl = req.body.imageUrl
-  const updatedDesc = req.body.description
+// exports.getPostEditProducts = async (req,res,next) => {
+//   const prodId =  req.body.productId
+//   const updatedTitle = req.body.title
+//   const updatedPrice = req.body.price
+//   const updatedImageUrl = req.body.imageUrl
+//   const updatedDesc = req.body.description
 
-try {
-  await Product.update({ title: updatedTitle, price: updatedPrice, imageUrl: updatedImageUrl, description: updatedDesc }, {
-    where: {
-      id: prodId
-    }
-  })
-  res.redirect('/admin/products')
-} catch(err) {
-  console.log(err)
-}
+// try {
+//   await Product.update({ title: updatedTitle, price: updatedPrice, imageUrl: updatedImageUrl, description: updatedDesc }, {
+//     where: {
+//       id: prodId
+//     }
+//   })
+//   res.redirect('/admin/products')
+// } catch(err) {
+//   console.log(err)
+// }
 
-}
+// }
 
-exports.getAllProducts = (req,res,next) => {
-  Product.findAll()
-  .then((products) => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Products',
-      path: '/admin/products',
-    });
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-}
+// exports.getAllProducts = (req,res,next) => {
+//   Product.findAll()
+//   .then((products) => {
+//     res.render('admin/products', {
+//       prods: products,
+//       pageTitle: 'Products',
+//       path: '/admin/products',
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+// }
 
-exports.deleteProduct = (req,res,next) => {
-  const prodId =  req.body.productId
-  Product.destroy({
-    where: {
-      id: prodId
-    }
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-  res.redirect('/admin/products')
-}
+// exports.deleteProduct = (req,res,next) => {
+//   const prodId =  req.body.productId
+//   Product.destroy({
+//     where: {
+//       id: prodId
+//     }
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+//   res.redirect('/admin/products')
+// }
--- routes/admin.js
+++ routes/admin.js
@@ -6,21 +6,21 @@ const router = express.Router();
 const adminController = require('../controller/admin')
 
 
-// /admin/add-product => GET
+// // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProducts);
 
-// /admin/products => GET
-router.get('/products', adminController.getAllProducts);
+// // /admin/products => GET
+// router.get('/products', adminController.getAllProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProducts);
 
-//edit product
-router.get('/edit-product/:productId', adminController.getEditProducts)
+// //edit product
+// router.get('/edit-product/:productId', adminController.getEditProducts)
 
-//post request to edit product
-router.post('/edit-product', adminController.getPostEditProducts)
+// //post request to edit product
+// router.post('/edit-product', adminController.getPostEditProducts)
 
-router.post('/delete-product', adminController.deleteProduct)
+// router.post('/delete-product', adminController.deleteProduct)
 
 module.exports = router
