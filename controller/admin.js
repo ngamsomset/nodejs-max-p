ee942a123ceff9f4205aa18f2b781f4a688e124f
ngamsom <setthasiri.ngm@gmail.com>
2023-01-22 09:25:35 +1100

    pass the user into the new product

--- controller/admin.js
+++ controller/admin.js
@@ -5,7 +5,7 @@ exports.postAddProducts = (req, res, next) => {
     const imageUrl = req.body.imageUrl
     const price = req.body.price
     const description = req.body.description
-    const product = new Product(title, price, description, imageUrl)
+    const product = new Product(title, price, description, imageUrl, null, req.user._id)
     product.save()
     .then((result) => {
       // console.log(result)
