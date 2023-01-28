f81aea46545202d5e8f37a69aae419bbafb10120
ngamsom <setthasiri.ngm@gmail.com>
2023-01-29 09:03:44 +1100

    setting cookies

--- controller/auth.js
+++ controller/auth.js
@@ -1,12 +1,16 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").split("=")[1].trim();
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  //this is not working!!. Even we set the req.isLoggedIn = true but right after we send the respond.
+  //the req is no longer there because the respond already been throw, thus terminated.
+  // req.isLoggedIn = true;
+  res.setHeader("Set-Cookie", "loggedIn=true");
   res.redirect("/");
 };
